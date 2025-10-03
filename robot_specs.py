99e2bb4c06e30afb281d950e7469731b2ff2d03b
Akbarbek Xaytbayev <hajtbaevakbar1@gmail.com>
2025-10-03 21:48:46 +0500

    first commit

---
+++ robot_specs.py
@@ -0,0 +1,22 @@
+model = input("Robot model name:")
+number = input("Serial Number:")
+creator = input("Creator's Name:")
+source = input("Power Source:")
+function = input("Primary Function:")
+print("=======================================")
+print("        ROBOTICS DIVISION - SPEC SHEET")
+print("=======================================\n")
+
+print("Model:           {model}")
+print("Serial Number:   {number}")
+print("Creator:         {creator}\n")
+
+print("Full Designation: {creator}'s {model}\n")
+
+print("---------------------------------------")
+print("Primary Function: {function}")
+print("Power Source:     {source}")
+print("---------------------------------------\n")
+
+print("            STATUS: OPERATIONAL")
+print("=======================================")
