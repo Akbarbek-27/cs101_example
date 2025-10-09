58d4b9c6ccdb06c8a5d6f5728f551c1b9ecda0af
Akbarbek-27 <hajtbaevakbar1@gmail.com>
2025-10-09 20:17:40 +0500

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,7 @@
+celsius = float(input("Write temperature in Celsius: "))
+
+fahrenheit = float((celsius * 9 / 5 + 32))
+kelvin = float((celsius + 273.15))
+print(f"Celsius: {celsius:.2f}")
+print(f"Fahrenheit: {fahrenheit:.2f}")
+print(f"Kelvin: {kelvin:.2f}")
